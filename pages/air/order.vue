1df1a9e32f63dea212d95c94e6fa00976d59e2ed
Jocelyn-Fung <FengRuli2015@163.com>
2020-01-03 20:21:57 +0800

    完成了订单页面的静态资源加载，以及基础交互

---
+++ pages/air/order.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main">
+          <orderForm :data='form'/>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import orderForm from '@/components/air/orderForm'
+export default {
+    data(){
+        return{
+          form:{
+              seat_infos:{}
+          }
+        }
+    },
+    components:{
+        orderForm
+    },
+     mounted(){
+    //   console.log(this.$route.query)
+    //   getTicketData(seat_id){
+          this.$axios({
+              method:'get',
+              url:'/airs/'+ this.$route.query.id,
+              params:this.$route.query.seat_id
+          }).then(res=>{
+            //   console.log(res)
+            this.form = res.data
+          })
+    //   }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
